2f1b4252b65344d2553948d8dbd6199a451dc9df
maribservices <maribservices@gmail.com>
2025-10-19 02:52:40 +0300

    msg

--- marib-app/lib/ui/screens/home_screen/section/section_screen/widgets/filter_sort_bar/filter_sort_action_button.dart
+++ marib-app/lib/ui/screens/home_screen/section/section_screen/widgets/filter_sort_bar/filter_sort_action_button.dart
@@ -2,8 +2,6 @@ import 'package:flutter/material.dart';
 import 'package:marib/utils/extensions/extensions.dart';
 import 'package:marib/ui/theme/theme.dart';
 
-
-
 class FilterSortActionButton extends StatelessWidget {
   final VoidCallback onTap;
   final Widget icon;
@@ -31,58 +29,58 @@ class FilterSortActionButton extends StatelessWidget {
     final radius = BorderRadius.circular(borderRadius);
 
     return Material(
-        color: Colors.transparent,
-        elevation: 3,
-        shadowColor: highlightColor.withOpacity(0.18),
+      color: Colors.transparent,
+      elevation: 3,
+      shadowColor: highlightColor.withOpacity(0.18),
+      borderRadius: radius,
+      child: InkWell(
+        onTap: onTap,
         borderRadius: radius,
-        child: InkWell(
-          onTap: onTap,
-          borderRadius: radius,
-          splashColor: highlightColor.withOpacity(0.16),
-          child: Ink(
-            decoration: BoxDecoration(
-              borderRadius: radius,
-              gradient: LinearGradient(
-                begin: Alignment.topLeft,
-                end: Alignment.bottomRight,
-                colors: [
-                  baseColor.brighten(12),
-                  baseColor.darken(8),
+        splashColor: highlightColor.withOpacity(0.16),
+        child: Ink(
+          decoration: BoxDecoration(
+            borderRadius: radius,
+            gradient: LinearGradient(
+              begin: Alignment.topLeft,
+              end: Alignment.bottomRight,
+              colors: [
+                baseColor.brighten(12),
+                baseColor.darken(8),
+              ],
+            ),
+            border: Border.all(
+              color: highlightColor.withOpacity(0.32),
+              width: 1.2,
+            ),
+          ),
+          child: Padding(
+            padding: padding,
+            child: IntrinsicHeight(
+              child: Column(
+                mainAxisSize: MainAxisSize.min,
+                mainAxisAlignment: MainAxisAlignment.center,
+                crossAxisAlignment: CrossAxisAlignment.center,
+                children: [
+                  IconTheme.merge(
+                    data: IconThemeData(color: onBaseColor, size: 22),
+                    child: icon,
+                  ),
+                  SizedBox(height: iconTextSpacing),
+                  Text(
+                    label,
+                    maxLines: 2,
+                    textAlign: TextAlign.center,
+                    style: TextStyle(
+                      fontWeight: FontWeight.w600,
+                      color: onBaseColor,
+                    ),
+                  ),
                 ],
+              ),
             ),
-              border: Border.all(
-                color: highlightColor.withOpacity(0.32),
-                width: 1.2,
-            ),
-              child: Padding(
-                padding: padding,
-                child: IntrinsicHeight(
-                  child: Column(
-                    mainAxisSize: MainAxisSize.min,
-                    mainAxisAlignment: MainAxisAlignment.center,
-                    crossAxisAlignment: CrossAxisAlignment.center,
-                    children: [
-                      IconTheme.merge(
-                        data: IconThemeData(color: onBaseColor, size: 22),
-                        child: icon,
-                      ),
-                      SizedBox(height: iconTextSpacing),
-                      Text(
-                        label,
-                        maxLines: 2,
-                        textAlign: TextAlign.center,
-                        style: TextStyle(
-                          fontWeight: FontWeight.w600,
-                          color: onBaseColor,
-                        ),
-                      ),
-                    ],
-                ),
-                ),
-            ),
+          ),
         ),
       ),
-            ),
     );
   }
 }

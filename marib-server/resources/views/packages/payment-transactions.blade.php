e1b82458398000be67f3b9da65310f2f29206465
maribservices <maribservices@gmail.com>
2025-10-21 02:56:36 +0300

    msg

--- marib-server/resources/views/packages/payment-transactions.blade.php
+++ marib-server/resources/views/packages/payment-transactions.blade.php
@@ -43,7 +43,7 @@
                                         <th scope="col" data-field="id" data-align="center" data-sortable="true">{{ __('ID') }}</th>
                                         <th scope="col" data-field="user.name" data-align="center" data-sortable="false">{{ __('User Name') }}</th>
                                         <th scope="col" data-field="amount" data-align="center" data-sortable="false">{{ __('Amount') }}</th>
-                                        <th scope="col" data-field="payment_gateway" data-align="center">{{ __('Payment Gateway') }}</th>
+                                        <th scope="col" data-field="gateway_label" data-align="center">{{ __('Payment Gateway') }}</th>
                                         <th scope="col" data-field="payment_status" data-align="center" data-sortable="true">{{ __('Payment Status') }}</th>
                                         <th scope="col" data-field="created_at" data-align="center" data-sortable="true">{{ __('Created At') }}</th>
                                     </tr>
@@ -55,4 +55,98 @@
                 </div>
             </div>
         </div>
+@endsection
+
+
+
+@section('js')
+    <script>
+        (function () {
+            const normalizedChannelKey = 'normalized_channel';
+            const gatewayFieldAliases = ['gateway_label', 'payment_gateway'];
+            const originalQueryParams = window.queryParams ?? (p => p);
+
+            function parseFilterPayload(raw) {
+                if (raw === null || typeof raw === 'undefined' || raw === '') {
+                    return { value: null, serialized: false };
+                }
+
+                if (typeof raw === 'string') {
+                    try {
+                        const parsed = JSON.parse(raw);
+                        if (parsed && typeof parsed === 'object' && !Array.isArray(parsed)) {
+                            return { value: { ...parsed }, serialized: true };
+                        }
+                    } catch (error) {
+                        return { value: null, serialized: true };
+                    }
+
+                    return { value: null, serialized: true };
+                }
+
+                if (typeof raw === 'object' && !Array.isArray(raw)) {
+                    return { value: { ...raw }, serialized: false };
+                }
+
+                return { value: null, serialized: false };
+            }
+
+            function remapGatewayKey(container) {
+                if (!container?.value) {
+                    return false;
+                }
+
+                const filters = container.value;
+                let mutated = false;
+
+                gatewayFieldAliases.forEach(alias => {
+                    if (Object.prototype.hasOwnProperty.call(filters, alias)) {
+                        const value = filters[alias];
+                        if (typeof value !== 'undefined') {
+                            filters[normalizedChannelKey] = value;
+                        }
+                        delete filters[alias];
+                        mutated = true;
+                    }
+                });
+
+                return mutated;
+            }
+
+            function normalizeGroupBy(value) {
+                if (typeof value === 'string') {
+                    return gatewayFieldAliases.includes(value) ? normalizedChannelKey : value;
+                }
+
+                if (Array.isArray(value)) {
+                    return value.map(item => gatewayFieldAliases.includes(item) ? normalizedChannelKey : item);
+                }
+
+                return value;
+            }
+
+            window.queryParams = function (params) {
+                const initial = originalQueryParams(params) ?? {};
+                const next = { ...initial };
+
+                if (typeof next.sort === 'string' && gatewayFieldAliases.includes(next.sort)) {
+                    next.sort = normalizedChannelKey;
+                }
+
+                next.group_by = normalizeGroupBy(next.group_by);
+
+                const filterPayload = parseFilterPayload(next.filter);
+                if (remapGatewayKey(filterPayload)) {
+                    next.filter = filterPayload.serialized ? JSON.stringify(filterPayload.value) : filterPayload.value;
+                }
+
+                const filtersPayload = parseFilterPayload(next.filters);
+                if (remapGatewayKey(filtersPayload)) {
+                    next.filters = filtersPayload.serialized ? JSON.stringify(filtersPayload.value) : filtersPayload.value;
+                }
+
+                return next;
+            };
+        })();
+    </script>
 @endsection

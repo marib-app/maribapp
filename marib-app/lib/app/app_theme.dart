b57c393078a50d220d8499d30412f78c7715e816
maribservices <maribservices@gmail.com>
2025-10-20 03:52:58 +0300

    msg

--- marib-app/lib/app/app_theme.dart
+++ marib-app/lib/app/app_theme.dart
@@ -6,12 +6,35 @@ import 'package:marib/app/navigation/motion/motion_page_transitions_builder.dart
 
 enum AppTheme { dark, light }
 
+
+final _baseLightColorScheme = ColorScheme.fromSeed(
+  seedColor: territoryColor_,
+  brightness: Brightness.light,
+  error: errorMessageColor,
+);
+
+final _baseDarkColorScheme = ColorScheme.fromSeed(
+  seedColor: territoryColorDark,
+  brightness: Brightness.dark,
+  error: errorMessageColor.withOpacity(0.7),
+);
+
 final appThemeData = {
   AppTheme.light: ThemeData(
     // scaffoldBackgroundColor: pageBackgroundColor,
     brightness: Brightness.light,
     //textTheme
     useMaterial3: false,
+
+
+    scaffoldBackgroundColor: primaryColor_,
+    cardColor: cardColorLight,
+    appBarTheme: const AppBarTheme(
+      backgroundColor: primaryColor_,
+      elevation: 0,
+      foregroundColor: textDarkColor,
+      surfaceTintColor: Colors.transparent,
+    ),
 
     pageTransitionsTheme: const PageTransitionsTheme(
       builders: <TargetPlatform, PageTransitionsBuilder>{
@@ -36,15 +59,38 @@ final appThemeData = {
         if (states.contains(MaterialState.selected)) {
           return territoryColor_.withOpacity(0.3);
         }
-        return primaryColorDark;
+        return neutralStrokeLight;
       }),
     ),
-    colorScheme: ColorScheme.fromSeed(
-        error: errorMessageColor, seedColor: territoryColor_,brightness:Brightness.light),
+    colorScheme: _baseLightColorScheme.copyWith(
+      background: primaryColor_,
+      surface: cardColorLight,
+      primary: territoryColor_,
+      secondary: headingAccentLight,
+      tertiary: headingAccentLight,
+      outline: neutralStrokeLight,
+      onPrimary: Colors.white,
+      onSecondary: textDarkColor,
+      onTertiary: textDarkColor,
+      onBackground: textDarkColor,
+      onSurface: textDarkColor,
+    ),
+
+
   ),
   AppTheme.dark: ThemeData(
     brightness: Brightness.dark,
     useMaterial3: false,
+
+
+    scaffoldBackgroundColor: primaryColorDark,
+    cardColor: cardColorDark,
+    appBarTheme: const AppBarTheme(
+      backgroundColor: primaryColorDark,
+      elevation: 0,
+      foregroundColor: textColorDarkTheme,
+      surfaceTintColor: Colors.transparent,
+    ),
 
     pageTransitionsTheme: const PageTransitionsTheme(
       builders: <TargetPlatform, PageTransitionsBuilder>{
@@ -63,16 +109,28 @@ final appThemeData = {
       selectionColor: territoryColorDark,
       cursorColor: territoryColorDark,
     ),
-    colorScheme: ColorScheme.fromSeed(
-        error: errorMessageColor.withOpacity(0.7),
-        seedColor: territoryColorDark,brightness:Brightness.dark),
+    colorScheme: _baseDarkColorScheme.copyWith(
+      background: primaryColorDark,
+      surface: cardColorDark,
+      primary: territoryColorDark,
+      secondary: headingAccentDark,
+      tertiary: headingAccentDark,
+      outline: neutralStrokeDark,
+      onPrimary: Colors.white,
+      onSecondary: textColorDarkTheme,
+      onTertiary: textColorDarkTheme,
+      onBackground: textColorDarkTheme,
+      onSurface: textColorDarkTheme,
+    ),
+
+
     switchTheme: SwitchThemeData(
         thumbColor: const MaterialStatePropertyAll(territoryColor_),
         trackColor: MaterialStateProperty.resolveWith((states) {
           if (states.contains(MaterialState.selected)) {
             return territoryColor_.withOpacity(0.3);
           }
-          return primaryColor_.withOpacity(0.2);
+          return neutralStrokeDark;
         })),
   )
 };
--- marib-app/lib/ui/theme/theme.dart
+++ marib-app/lib/ui/theme/theme.dart
@@ -5,23 +5,32 @@ export 'extensions/shimmer_colors.dart';
 
 ///Light Theme Colors
 ///This color format is different, isn't it? .You can use hex colors here also but you have to remove '#' symbol and add 0xff instead.
-const Color primaryColor_ = Color(0xFFF6F5FA);
+const Color primaryColor_ = Color(0xFFF7F7F8);
 // const Color primaryColor_ = Color(0xFFABABAB);
 const Color secondaryColor_ = Color(0xFFFFFFFF);
 // const Color secondaryColor_ = Color(0xFF525252);
+const Color headingAccentLight = Color(0xFFA8D7E6);
+const Color headingAccentDark = Color(0xFF4FA5BA);
+const Color neutralStrokeLight = Color(0xFFE2E3E7);
+const Color neutralStrokeDark = Color(0xFF2A2A2D);
+
+
 
 // 0xffEB5924
 const Color territoryColor_ = Color.fromARGB(255, 235, 89, 36);
 const Color forthColor_ = Color.fromRGBO(250, 110, 83, 1);
 const Color _backgroundColor = primaryColor_; //here you can change if you need
 const Color textDarkColor = Color(0xFF000000);
-Color lightTextColor = const Color(0xFF000000).withOpacity(0.5);
+Color widgetsBorderColorLight = neutralStrokeLight;
 Color widgetsBorderColorLight = const Color(0xffEEEEEE).withOpacity(0.6);
 Color senderChatColor = const Color.fromARGB(255, 233, 233, 233).darken(22);
 
 ///Dark Theme Colors
-Color primaryColorDark = const Color(0xff121212);
-Color secondaryColorDark = const Color(0xff1C1C1C).brighten(5);
+Color primaryColorDark = const Color(0xFF0E0E10);
+Color secondaryColorDark = const Color(0xFF1E1E20);
+const Color cardColorLight = secondaryColor_;
+Color cardColorDark = secondaryColorDark.brighten(6);
+
 const Color territoryColorDark = Color(0xffEB5924);
 Color deactivateColorLight = const Color(0xff7F7F7F);
 const Color deactivateColorDark = Color(0xFF3D3D3D);
@@ -30,7 +39,7 @@ const Color forthColorDark = Color(0xffEB5924);
 Color backgroundColorDark = primaryColorDark; //here you can change if you need
 const Color textColorDarkTheme = Color(0xffFDFDFD);
 Color lightTextColorDarkTheme = const Color(0xffFDFDFD).withOpacity(0.3);
-Color widgetsBorderColorDark = const Color(0x1aFDFDFD);
+Color widgetsBorderColorDark = neutralStrokeDark;
 //Color popUpColor = const Color(0xff02AD11);
 Color darkSenderChatColor =
     const Color.fromARGB(255, 233, 233, 233).darken(100);
@@ -62,11 +71,21 @@ extension ColorPrefs on ColorScheme {
   Color get secondaryColor => _getColor(brightness,
       lightColor: secondaryColor_, darkColor: secondaryColorDark);
 
+  Color get cardColor =>
+      _getColor(brightness, lightColor: cardColorLight, darkColor: cardColorDark);
+
+
   Color get secondaryDetailsColor => _getColor(brightness,
       lightColor: secondaryColor_, darkColor: primaryColorDark);
 
   Color get territoryColor => _getColor(brightness,
       lightColor: territoryColor_, darkColor: territoryColorDark);
+
+
+  Color get headingAccentColor => _getColor(brightness,
+      lightColor: headingAccentLight, darkColor: headingAccentDark);
+
+
 
   Color get deactivateColor => _getColor(brightness,
       lightColor: deactivateColorLight, darkColor: deactivateColorDark);
@@ -92,8 +111,11 @@ extension ColorPrefs on ColorScheme {
       lightColor: lightTextColor, darkColor: lightTextColorDarkTheme);
 
   Color get borderColor => _getColor(brightness,
-      lightColor: widgetsBorderColorLight,
-      darkColor: secondaryColorDark.withOpacity(0.2));
+      lightColor: neutralStrokeLight,
+      darkColor: neutralStrokeDark);
+
+  Color get neutralStrokeColor => _getColor(brightness,
+      lightColor: neutralStrokeLight, darkColor: neutralStrokeDark);
 
   Color get chatSenderColor => _getColor(brightness,
       lightColor: senderChatColor, darkColor: darkSenderChatColor);

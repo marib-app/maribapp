47ce7c176de338b524c9cc2e58c03a05c63f3e75
maribservices <maribservices@gmail.com>
2025-10-20 08:48:48 +0300

    msg

--- marib-server/app/Http/Controllers/ManualPaymentRequestController.php
+++ marib-server/app/Http/Controllers/ManualPaymentRequestController.php
@@ -2310,13 +2310,13 @@ class ManualPaymentRequestController extends Controller
         if ($manualGatewayAliasesForFilter === []) {
             $manualGatewayAliasesForFilter = ['manual_bank', 'manual_banks'];
         }
-
         $manualGatewayAliasesForFilter = array_values(array_unique(array_filter(array_merge(
             $manualGatewayAliasesForFilter,
             $walletGatewayAliases
-        ), static fn ($alias) => is_string($alias) && $alias !== ''))));
+        ), static fn ($alias) => is_string($alias) && trim($alias) !== '')));
 
-        
+
+
         $transactions = DB::table('payment_transactions as pt')
             ->leftJoin('manual_payment_requests as mpr', function ($join) {
                 $join->on('pt.manual_payment_request_id', '=', 'mpr.id');

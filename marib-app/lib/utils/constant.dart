742484f3c3eaf5a7ecf9951cae20154e437b1d6c
maribservices <maribservices@gmail.com>
2025-10-23 02:52:40 +0300

    msg

--- marib-app/lib/utils/constant.dart
+++ marib-app/lib/utils/constant.dart
@@ -111,11 +111,9 @@ class Constant {
   static const int publicRootCategoryId = 6;
   static const int storeRootCategoryId = 6;
 
-
   static const int accountTypeCustomer = 1;
   static const int accountTypeRealEstate = 2;
   static const int accountTypeSeller = 3;
-
 
   static List<PostedSinceItem> postedSince = [
     PostedSinceItem(status: "All Time", value: "all-time"),

6ca277641a758c062355b616444eb15537b90c0a
maribservices <maribservices@gmail.com>
2025-11-08 16:33:11 +0300

    msg

---
+++ marib-server/tmp_call.php
@@ -0,0 +1,10 @@
+﻿<?php
+require __DIR__.'/vendor/autoload.php';
+$app = require __DIR__.'/bootstrap/app.php';
+$app->loadEnvironmentFrom('.env');
+$app->make(Illuminate\Contracts\Console\Kernel::class)->bootstrap();
+$request = Illuminate\Http\Request::create('/api/get-featured-section', 'GET');
+$kernel = $app->make(Illuminate\Contracts\Http\Kernel::class);
+$response = $kernel->handle($request);
+echo $response->getStatusCode(), "\n";
+echo $response->getContent();
